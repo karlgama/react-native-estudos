48359fda045ec2c17fe2788ebea9d5059ac674d3
kaique <kaiquesilva49@hotmail.com>
2022-10-10 17:39:50 -0300

    usando flat list

--- App.js
+++ App.js
@@ -3,17 +3,17 @@ import { SafeAreaView, StatusBar, Text, View } from 'react-native';
 import Cesta from './src/telas/Cesta';
 import { useFonts, Montserrat_400Regular, Montserrat_700Bold } from '@expo-google-fonts/montserrat';
 import mock from './src/mocks/cesta'
-import SplashScreen from 'expo-splash-screen';
+import AppLoading from 'expo-app-loading';
 
 export default function App() {
   const [fontCarregada] = useFonts({
     "MontserratRegular": Montserrat_400Regular,
     "MontserratBold": Montserrat_700Bold
   });
-  if (!fontCarregada) return <SplashScreen.preventAutoHideAsync />
+  if (!fontCarregada) return <AppLoading />
 
   return (
-    <SafeAreaView >
+    <SafeAreaView style={{ flex: 1 }}>
       <StatusBar />
       <Cesta {...mock} />
     </SafeAreaView>
--- src/telas/Cesta/index.js
+++ src/telas/Cesta/index.js
@@ -1,27 +1,43 @@
 import React from 'react'
-import { ScrollView, StyleSheet, View } from 'react-native';
+import { FlatList, StyleSheet, View } from 'react-native';
+import Texto from '../../componentes/Texto';
 import Detalhes from './componentes/Detalhes';
 import Topo from './componentes/Topo';
-import Itens from './itens';
+import Item from './item';
 
 function Cesta({ topo, detalhes, itens }) {
-    return <ScrollView>
-        <Topo {...topo} />
+    return <>
+        <FlatList data={itens.lista}
+            renderItem={Item}
+            keyExtractor={({ nome }) => nome}
+            ListHeaderComponent={() => {
+                return <>
+                    <Topo {...topo} />
+                    <View style={estilos.cesta}>
+                        <Detalhes {...detalhes} />
+                    </View>
+                    <Texto style={estilos.titulo}>{itens.titulo}</Texto>
+                </>
+            }}
+        />
 
-        <View style={estilos.cesta}>
-            <Detalhes {...detalhes} />
-            <Itens {...itens} />
-        </View>
-    </ScrollView>
+    </>
 }
 
 const estilos = StyleSheet.create({
-
     cesta: {
         paddingVertical: 8,
         paddingHorizontal: 16
     },
-
+    titulo: {
+        color: "#464646",
+        fontWeight: 'bold',
+        marginTop: 32,
+        marginBottom: 8,
+        fontSize: 20,
+        lineHeight: 32,
+        paddingLeft: 16
+    },
 });
 
 export default Cesta;
